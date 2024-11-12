12da30f6758b4ff9bdc1445935966825bef2b098
Saray Tamayo <maura.tamayom@javeriana.edu.co>
2024-11-11 23:55:50 -0500

    feature detail Room

--- src/views/booking.js
+++ src/views/booking.js
@@ -34,6 +34,9 @@ const Booking = () => {
 
   const handlePay = (reservation) => { router.push(`/payment?reservation=${encodeURIComponent(JSON.stringify(reservation))}`); };
 
+  const handleDetailRoom = (id) => { router.push(`/room/${id}`); };
+
+
   if (!reservations.length) {
     return <Typography variant="h6">Cargando...</Typography>;
   }
@@ -46,13 +49,16 @@ const Booking = () => {
         Lista de Reservas
       </Typography>
       {reservations.map((reservation, index) => (
-        <Card key={index} sx={{ display: 'flex', flexDirection: 'column', mb: 4 }}>
+        <Card key={reservation.id} sx={{ display: 'flex', flexDirection: 'column', mb: 4 }}>
           <CardContent>
+           
             <Typography variant="h5" fontWeight="bold">Habitación: {reservation.room.nombre}</Typography>
             <Typography variant="body2" color="text.secondary">Precio por noche: {reservation.room.precio_por_noche}</Typography>
             <Typography variant="body2" color="text.secondary">Cantidad de dias: {reservation.dias_reservados}</Typography>
             <Typography variant="body2" color="text.secondary">Número de habitaciones: {reservation.room.numero_de_camas}</Typography>
             <Typography variant="body2" color="text.secondary">Estado: {reservation.pagado ? 'Pagada' : 'Pendiente'}</Typography>
+        
+            
             {reservation.payment.fecha && (
               <>
                 <Typography variant="body2" color="text.secondary">Fecha de pago: {reservation.payment.fecha}</Typography>
@@ -60,12 +66,19 @@ const Booking = () => {
                 <Typography variant="body2" color="text.secondary">ID de transacción: {reservation.payment.id_transaccion}</Typography>
               </>
             )}
+            
             <Divider sx={{ my: 2 }} />
             {!reservation.pagado && (
               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Button variant="contained" color="primary" onClick={() => handlePay(reservation)}>
-                  Pagar
-                </Button>
+                <Box>
+                  <Button variant="contained" color="primary" onClick={() => handlePay(reservation)}>
+                    Pagar
+                  </Button>
+                  <Button sx={{margin: "0 10px", background:"#070F1B" }} variant="contained" onClick={() => handleDetailRoom(reservation.id)}>
+                    Ver mas Detalle
+                  </Button>
+                </Box>
+                
                 <Button variant="contained" color="secondary" onClick={() => handleDelete(index)}>
                   Eliminar
                 </Button>
